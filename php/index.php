558ff7969525018ca9fd7f3bf69460ca185fce3e
Claudio Benvenuti <claudio@benve.it>
2024-03-20 12:23:22 +0100

    fix

--- php/index.php
+++ php/index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-  $mysqli_connection = new MySQLi('db', 'root', 'ciccio', 'db');
+  $mysqli_connection = new MySQLi('my_db', 'root', 'ciccio', 'db');
   $result = $mysqli_connection->query("SELECT * FROM alunni");
   $results = $result->fetch_all(MYSQLI_ASSOC);
   $response->getBody()->write(json_encode($results));
