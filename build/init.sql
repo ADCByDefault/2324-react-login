a959decd571c87338648f64a75179b9c97d023fa
Claudio Benvenuti <claudio@benve.it>
2024-05-04 15:34:45 +0200

    backend

--- build/init.sql
+++ build/init.sql
@@ -1,15 +1,8 @@
-CREATE TABLE `alunni` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(20) NOT NULL,
-  `cognome` varchar(20) NOT NULL
+CREATE TABLE `partita` (
+  `id` varchar(20) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `tentativi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `alunni` (`id`, `nome`, `cognome`) VALUES
-(1, 'claudio', 'benve'),
-(2, 'ivan', 'bruno');
-
-ALTER TABLE `alunni`
-ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `alunni`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `partita` ADD PRIMARY KEY (id);
+ALTER TABLE `partita` ADD UNIQUE (id);
--- php/index.php
+++ php/index.php
@@ -2,10 +2,11 @@
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/AlunniController.php';
+require __DIR__ . '/controllers/PartiteController.php';
 
 $app = AppFactory::create();
 
-$app->get('/alunni', "AlunniController:index");
+$app->post('/partita', "PartiteController:create");
+$app->put('/partita/{id}', "PartiteController:update");
 
 $app->run();
